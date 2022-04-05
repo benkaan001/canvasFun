268edc4989e09491136ff403fae651acf8d701d1
ben kaan <benkaan001@gmail.com>
2022-04-05 11:35:12 -0500

    update Projectile logic

--- app.js
+++ app.js
@@ -46,32 +46,14 @@ const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
-// const projectile1 = new Projectile(
-//   canvas.width / 2,
-//   canvas.height / 2,
-//   5,
-//   'red',
-//   {
-//     x: 1,
-//     y: 1,
-//   }
-// );
-// const projectile2 = new Projectile(
-//   canvas.width / 2,
-//   canvas.height / 2,
-//   5,
-//   'green',
-//   {
-//     x: -1,
-//     y: -1,
-//   }
-// );
 const projectiles = [];
 
 function animate() {
   requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // to ensure the player is not being cleared by clearRect
+  player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
   });
@@ -81,12 +63,13 @@ window.addEventListener('click', (event) => {
     event.clientY - canvas.height / 2,
     event.clientX - canvas.width / 2
   );
-  console.log(angle);
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  // create projectiles dynamically
   projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
-      x: 1,
-      y: 1,
-    })
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
   );
 });
 
