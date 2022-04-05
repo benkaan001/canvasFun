ae37e180d6a6f6403922dd87a93f8bd3520a52d7
ben kaan <benkaan001@gmail.com>
2022-04-05 12:14:22 -0500

    add Enemy class

--- app.js
+++ app.js
@@ -41,6 +41,28 @@ class Projectile {
     this.draw();
   }
 }
+// create Enemy class using Projectile class
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.draw();
+  }
+}
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -48,6 +70,37 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, 'blue');
 
 const projectiles = [];
+const enemies = [];
+
+// create release enemies function
+function spawnEnemies() {
+  setInterval(() => {
+    // randomize the size of the enemy
+    const radius = Math.random() * (30 - 4) + 4;
+
+    // randomize the x and y coordinates of the enemy source location
+    let x;
+    let y;
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    // randomize the enemy colors
+    let colors = ['green', 'orange', 'purple', 'pink'];
+    const color = colors[Math.floor(Math.random() * (colors.length - 0) + 0)];
+    // reverse the projectile angle to ensure enemies are moving towards the center
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -56,6 +109,9 @@ function animate() {
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 window.addEventListener('click', (event) => {
@@ -74,3 +130,4 @@ window.addEventListener('click', (event) => {
 });
 
 animate();
+spawnEnemies();
